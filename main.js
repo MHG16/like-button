cfd4b40ae4dcdad3dc330ca2bdb1ec2c23af764b
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-15 12:57:04 -0500

    Like button working

---
+++ main.js
@@ -0,0 +1,35 @@
+
+//1. Target the element we're trying to listen for the event on
+//2. Tell a function to run when that event happens
+//3. Create the function 
+
+var buttonEl = document.querySelector('.btnLike');
+var likes = 0;
+
+buttonEl.addEventListener('click', updateButton)
+
+function updateButton() {
+	likes++;
+
+	buttonEl.innerHTML = likes+' likes';
+}
+
+//create another button that when pressed, resets the like counter back to 0
+
+var resetButtonEl = document.querySelector('.btnReset');
+
+resetButtonEl.addEventListener('click', updateLikes)
+
+function updateLikes() {
+	likes = 0;
+	buttonEl.innerHTML = likes+' likes';
+}
+
+
+
+
+
+
+
+
+
